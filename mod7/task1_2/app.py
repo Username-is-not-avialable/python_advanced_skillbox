f16fc1927b5e09d13229dcc4cf3ff5d662db21b0
minty <antonbakir280@mail.ru>
2024-04-02 05:12:43 +0500

    mod 7 task 3 improvements

--- mod7/task1_2/app.py
+++ mod7/task1_2/app.py
@@ -32,7 +32,6 @@ def process_command(command: str) -> tuple[float, float, str] | None:
 
 def get_result(command: tuple[float, float, str] | None) -> str:    
     if command is None:
-        calc_logger.info("Введена некорректная команада")
         return "Вы ввели не корректную строку, повторите попытку."
     number_1, number_2, operation = command    
     result: float = utils.calculate(number_1, number_2, operation)
@@ -44,40 +43,29 @@ def give_result_to_user(result: str) -> None:
 
 def configure_logger(logger: logging.Logger):
     formatter = logging.Formatter("%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s")
-    level_handler = CustomHandler(calc_logger)
+    level_handler = CustomHandler()
     level_handler.setFormatter(formatter)
     logger.addHandler(level_handler)
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format="%(levelname)s | %(name)s | %(asctime)s | %(lineno)d | %(message)s")
 
 
 class CustomHandler(logging.Handler):
-    def __init__(self, loger_name: str, level: int, mode='a'):
+    def __init__(self, mode='a'):
         super().__init__()
-        self.loger_name = loger_name
         self.mode = mode
-        self.level = level
 
     def emit(self, record: logging.LogRecord) -> None:
         message = self.format(record)
-
-        match self.level:
-            case logging.DEBUG:
-                output_file: str = self.loger_name + '_debug.log'
-            case logging.INFO:
-                output_file: str = self.loger_name + '_info.log'
-            case logging.WARNING:
-                output_file: str = self.loger_name + '_warning.log'
-            case logging.ERROR:
-                output_file: str = self.loger_name + '_error.log'
-            case logging.CRITICAL:
-                output_file: str = self.loger_name + '_critical.log'
+        loger_name:str = record.name
+        loger_level:str = str.lower(record.levelname)
+        output_file:str = f"{loger_name}_{loger_level}.log"
 
         with open(output_file, mode=self.mode) as f:
             f.write(message + '\n')
 
 
 if __name__ == '__main__':
-    configure_logger()
+    configure_logger(calc_logger)
     show_list_of_commands()
     while True:
         command: str = get_command_from_user()
